485ee7bb252a3ddfaa6dc95c9d9510fd9207df12
Sanyogita Purandare <47488309+snygt2007@users.noreply.github.com>
2019-07-03 00:54:48 -0700

    The files are as per the read me document
    The files are as per the read me document

---
+++ Insight_Project_Framework/Create_supervised_data.py
@@ -0,0 +1,45 @@
+from Library_create_supervised_data import *
+
+
+
+dir_litw_resized=Path("./data/resized_images_details")
+dir_litw =Path("./data/Raw_data")
+dir_litw_super_clean=Path("./data/super_images_cleaned")
+
+
+df_logos_folder_label = pd.read_csv('.\data\Resized_folder_details.csv')
+tp_df_image_resized = pd.read_csv('.\data\Resized_file_details.csv')
+
+
+
+
+df_logos_folder_label.head()
+tp_df_image_resized.head()
+
+
+
+
+
+df_logos_folder=get_folder_stats(df_logos_folder_label, tp_df_image_resized,dir_litw_resized)
+
+
+
+A=0.5
+B=0.5
+C=0.4
+
+tp_df_excluded=test_data_exclusion_1(df_logos_folder,A,B,C, dir_litw_super_clean,dir_litw_resized)
+
+
+
+Print_filtered_folder_details(dir_litw_super_clean)
+
+
+
+
+tp_df_image_resized.to_pickle('image_logos_folder_path_7022019.pkl')
+
+
+
+
+
